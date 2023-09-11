15c58af2fca1acd625929c80ccf436228c1119ac
andreycoutt <contato.andreyyy@gmail.com>
2023-09-11 19:47:37 -0300

    v.0.0.1

---
+++ Rafael Mesquita - JavaScript/Calcular Imposto de renda v.0.0.1.js
@@ -0,0 +1,13 @@
+var salario_bruto = 7500;
+
+console.log("salário:", salario_bruto);
+
+var imposto_de_renda = salario_bruto * 10/100;
+
+var salario_liquido = salario_bruto - imposto_de_renda;
+
+console.log("Seu imposto de renda: R$", imposto_de_renda);
+console.log("Salário liquido: R$", salario_liquido);
+
+
+
