9f6812e480f9198dc52662d5876d4cf48c8e9b2f
andreycoutt <contato.andreyyy@gmail.com>
2023-09-11 19:28:10 -0300

    Operadores

---
+++ Rafael Mesquita - JavaScript/Aula 05 - Operadores.js
@@ -0,0 +1,46 @@
+x = 52;
+y = 24;
+
+soma = x + y;
+subtracao = x - y;
+multiplicacao = x * y;
+quociente = x / y;
+resto = x % y;
+potencia = x ** y;
+
+
+console.log("X:",x, "Y:",y,);
+console.log("Soma:", soma);
+console.log("Subtração:", subtracao);
+console.log("Multiplicação:", multiplicacao);
+console.log("Quociente:", quociente);
+console.log("Resto:", resto);
+console.log("Potência:", potencia);
+
+console.log("\n\n");
+console.log("operadores relacionais (demonstração)");
+
+x = 10;
+y = 20;
+z = "10";
+
+console.log("x:",x, "y:",y, "z",z,);
+console.log("x == z:", x == z);
+console.log("x != z:", x != z);
+console.log("x === z:", x === z);
+console.log("x !== z:", x !== z);
+console.log("x < y", x < y);
+console.log("x > y", x > y);
+console.log("x <= y", x <= y);
+console.log("x >= y", x >= y);
+
+console.log("\n\n");
+console.log("operadores lógicos (demonstração)");
+
+x = true;
+y = false;
+
+console.log("x =", x, "y =", y);
+console.log("x || y:", x || y);
+console.log("x && y:", x && y);
+console.log("! x:", !x);
