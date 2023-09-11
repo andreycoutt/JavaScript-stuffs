949a16b2ddea6f8d03739a2b303b6d5b542b8c80
andreycoutt <contato.andreyyy@gmail.com>
2023-09-11 18:52:19 -0300

    Variaveis

---
+++ Rafael Mesquita - JavaScript/Aula 04 - Exercicios.js
@@ -0,0 +1,17 @@
+idade = 20;
+altura = 1.75;
+nome = "José";
+pagou = true;
+
+
+console.log("os valores atribuidos as variaveis são: ");
+console.log("Idade: ", idade);
+console.log("Altura: ", altura);
+console.log("Nome: ", nome);
+console.log("Pagou: ", pagou);
+
+console.log("\nos tipos das variaveis são: \n");
+console.log("Idade: ", typeof(idade));
+console.log("Altura: ", typeof(altura));
+console.log("Nome: ", typeof(nome));
+console.log("Pagou: ", typeof(pagou));
---
+++ Rafael Mesquita - JavaScript/Aula 04 - variaveis.js
@@ -0,0 +1,17 @@
+nome = "maria";
+idade = 51;
+
+console.log("Nome", nome, "idade:", idade);
+
+
+nome = "josé";
+idade = 41;
+profissao = "desenvolvedor";
+
+console.log("\nnome: ",nome, "\nidade: ",idade, "\nprofissão: ",profissao);
+
+
+
+var x = 41 // variavel de escopo global ou local
+y =  42 // variavel de escopo global
+let z = 43 // variavel de escopo de bloco
