6d221f8c038c30aa968a4d6aa68687d23c0166f7
andreycoutt <contato.andreyyy@gmail.com>
2023-09-11 17:56:19 -0300

    fundamentos.

---
+++ Hora de Codar/fundamentos.js
@@ -0,0 +1,38 @@
+// Fundamentos
+
+var minhaVariavel = "ola mundo";
+
+console.log(minhaVariavel);
+
+var meuNumero = 10;
+
+console.log(meuNumero + 5);
+
+console.log("10" + 5);
+
+console.log(typeof meuNumero);
+
+var booleano = true; // ou false
+
+console.log(typeof booleano);
+
+var meuObjeto = {};
+var meuArray = [];
+var meuNull = null;
+var meuUnderfined = undefined;
+
+console.log(typeof meuObjeto);
+console.log(typeof meuArray);
+console.log(typeof meuNull);
+console.log(typeof meuUnderfined);
+
+// let e const
+// novas formas de declarar variaveis
+
+let x = 10;
+const y = 5;
+
+
+console.log(typeof x);
+
+console.log(x, y);
