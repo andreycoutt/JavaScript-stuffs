a99720a79147e658d874c14b5414d21b3f3cd104
andreycoutt <contato.andreyyy@gmail.com>
2023-09-11 21:54:11 -0300

    Entrada de Dados

---
+++ Rafael Mesquita - JavaScript/Aula 06 - Entrada de Dados.js
@@ -0,0 +1,11 @@
+const prompt = require('prompt-sync')();
+
+x = parseInt(prompt("Digite o número 1: "));
+y = parseInt(prompt("Digite o número 2: "));
+
+
+soma = x + y;
+
+
+console.log("\nx =", x, "\ny =", y);
+console.log("\nSoma =", soma);
